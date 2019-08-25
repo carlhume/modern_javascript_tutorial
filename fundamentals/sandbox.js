c528322a7a69c4eda0ff0b17bd88cd2c7bf5b550
carl <carl.hume@gmail.com>
2019-08-25 12:55:37 -0400

    Javascript variable assignment is by value

---
+++ fundamentals/sandbox.js
@@ -0,0 +1,7 @@
+let origin_message = 'I am the original message';
+let copied_message = origin_message;
+alert(origin_message);
+alert(copied_message);
+origin_message = 'I am the original message modified';
+alert(origin_message);
+alert(copied_message);
