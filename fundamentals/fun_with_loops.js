a39bd47227fd0ec5a42cc2dccab021dd773c47d6
carl <carl.hume@gmail.com>
2019-08-25 19:19:48 -0400

    Now finding all primes up to a specified ceiling.

--- fundamentals/fun_with_loops.js
+++ fundamentals/fun_with_loops.js
@@ -14,8 +14,28 @@ function promptForMoreThan100SupportingCancel() {
     const LARGEST_VALUE_STILL_LOOPING = 100;
     let userNumber = LARGEST_VALUE_STILL_LOOPING;
     while ( userNumber != undefined && userNumber != '' && userNumber <= LARGEST_VALUE_STILL_LOOPING ) {
-
-        userNumber = prompt( 'Enter a number larger than ' + LARGEST_VALUE_STILL_LOOPING, LARGEST_VALUE_STILL_LOOPING + 1 );
-        
+        userNumber = prompt( 'Enter a number larger than ' + LARGEST_VALUE_STILL_LOOPING, LARGEST_VALUE_STILL_LOOPING + 1 );        
     }
+
+}
+
+function alertPrimeNumbersUpTo( ceiling ) {
+
+    alert( `Alert with prime numbers up to ${ceiling}` );
+    for( let i = 0; i <= ceiling; i++ ) {
+        if( isPrime( i ) ) {
+            alert( i );
+        }
+    } 
+
+}
+
+function isPrime( aNumber ) {
+    let aNumberIsPrime = true;
+    for( let i=2; i<aNumber; i++ ) {
+        if( aNumber % i == 0 ) {
+            aNumberIsPrime = false;
+        }
+    }
+    return aNumberIsPrime;
 }
